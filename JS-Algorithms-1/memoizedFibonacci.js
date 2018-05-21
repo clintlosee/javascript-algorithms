b7f47a652ccea5f5b37df2b8784205a87c1de0d8
Clint Losee <clint.losee@rcwilley.com>
2018-05-21 11:57:58 -0600

    Added two version of fibonacci algorithm

---
+++ JS-Algorithms-1/memoizedFibonacci.js
@@ -0,0 +1,15 @@
+function memoizedFibonacci(index, cache) {
+    cache = cache || [];
+
+    if (cache[index]) return cache[index];
+    else {
+      if (index < 3) return 1;
+      else {
+        cache[index] = memoizedFibonacci(index - 1, cache) + memoizedFibonacci(index - 2, cache);
+      }
+    }
+
+    return cache[index];
+  }
+
+  memoizedFibonacci(45);
