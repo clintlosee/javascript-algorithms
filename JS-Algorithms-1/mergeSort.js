5859a44fedaeb517972c7db824a94783ef6df031
Clint Losee <clint.losee@gmail.com>
2018-05-23 11:01:47 -0600

    Remove misc text I didn't mean to leave there.

--- JS-Algorithms-1/mergeSort.js
+++ JS-Algorithms-1/mergeSort.js
@@ -30,9 +30,3 @@ function merge(array1, array2) {
 
 mergeSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 1, 9, 1]);
 mergeSort([100, -20, 40, -30, 16, -100, -101]);
-
-old
-Dining room sets & dining table and chair set at RC Willey.
-
-new
-Browse over 150 table and chair dining sets for your home. Buy your new dining room set from RC Willey, your dining room furniture store. <a href="https://www.rcwilley.com/Furniture/Dining-Room/Dining-Sets/Standard/Search.jsp">Browse standard height dining sets,</a> or if you prefer a taller dining experience, we also sell <a href="https://www.rcwilley.com/Furniture/Dining-Room/Dining-Sets/Counter-Height/Search.jsp">counter height dining sets.</a> For those of you who prefer a more intimate dining room set, take a moment and browse our <a href="https://www.rcwilley.com/Furniture/Dining-Room/Dining-Sets/Round/Search.jsp">round dining table sets.</a>
